fc8230d37ee2706e780ced6f0765b75c56265d86
Veritas <ochigbogodswill868@gmail.com>
2025-07-28 16:48:00 -0700

    please work

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -138,13 +138,14 @@ const submit = () => {
                     Already registered?
                 </a>
 
-                <PrimaryButton
-                    class="ms-4"
+                <button
+                    type="submit"
+                    class="ms-4 bg-gradient-to-r from-amber-400 to-yellow-500 hover:from-amber-500 hover:to-yellow-600 disabled:from-gray-300 disabled:to-gray-400 text-white px-8 py-3 rounded-xl transition-all duration-300 transform hover:scale-105 font-semibold text-lg shadow-lg"
                     :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing"
                 >
                     Register
-                </PrimaryButton>
+                </button>
             </div>
              <div class="flex justify-center">
                 <p class="mt-4 text-sm text-gray-600">
