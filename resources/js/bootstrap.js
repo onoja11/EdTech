fbd8dcdcdfd92aa66a3016e2db22ca3f87133b99
Veritas <ochigbogodswill868@gmail.com>
2025-07-28 16:02:55 -0700

    please work

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.baseURL = 'https://edtech-52ei.onrender.com'; 
+window.axios.defaults.baseURL = window.location.origin; 
 
 
 // window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
